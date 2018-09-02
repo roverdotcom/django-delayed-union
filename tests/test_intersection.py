60eacdf42a3473d56052e77c8eba0f71d1515cc8
Mike Hansen <mike@rover.com>
2018-09-02 11:50:53 +0200

    Add support for nested intersections

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -18,7 +18,14 @@ class DelayedIntersectionQuerySetMetaTests(
 
 
 class DelayedIntersectionQuerySetTestsMixin(DelayedQuerySetTestsMixin):
-    pass
+    def test_select_related(self):
+        base_qs = Permission.objects.all()
+        qs = DelayedIntersectionQuerySet(base_qs, base_qs)
+
+        self.assertTrue(qs.exists())
+        for permission in qs.select_related('content_type'):
+            with self.assertNumQueries(0):
+                self.assertIsNotNone(permission.content_type.id)
 
 
 class DelayedIntersectionQuerySetTests(
@@ -28,23 +35,35 @@ class DelayedIntersectionQuerySetTests(
     @classmethod
     def setUpTestData(cls):
         super(DelayedIntersectionQuerySetTests, cls).setUpTestData()
-        cls.first_user = UserFactory.create()
-        cls.second_user = UserFactory.create()
+        cls.user_b, cls.user_c = UserFactory.create_batch(2)
 
     def get_queryset(self):
         return DelayedIntersectionQuerySet(
-            User.objects.exclude(id__in=[self.second_user.id]),
-            User.objects.exclude(id__in=[self.first_user.id])
+            User.objects.exclude(id=self.user_b.id),
+            User.objects.exclude(id=self.user_c.id)
         )
 
     def get_expected_models(self):
         return [self.user]
 
-    def test_select_related(self):
-        base_qs = Permission.objects.all()
-        qs = DelayedIntersectionQuerySet(base_qs, base_qs)
 
-        self.assertTrue(qs.exists())
-        for permission in qs.select_related('content_type'):
-            with self.assertNumQueries(0):
-                self.assertIsNotNone(permission.content_type.id)
+class NestedDelayedIntersectionQuerySetTests(
+        DelayedIntersectionQuerySetTestsMixin,
+        TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        super(NestedDelayedIntersectionQuerySetTests, cls).setUpTestData()
+        cls.user_b, cls.user_c, cls.user_d = UserFactory.create_batch(3)
+
+    def get_queryset(self):
+        return DelayedIntersectionQuerySet(
+            User.objects.exclude(id=self.user_b.id),
+            DelayedIntersectionQuerySet(
+                User.objects.exclude(id=self.user_c.id),
+                User.objects.exclude(id=self.user_d.id),
+            )
+        )
+
+    def get_expected_models(self):
+        return [self.user]
