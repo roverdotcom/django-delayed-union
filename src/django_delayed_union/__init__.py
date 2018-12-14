386928caa5cbb2d93e794f7b44f308cefafd6439
Mike Hansen <mike@rover.com>
2018-12-14 13:33:51 +0100

    Bump version: 0.1.1 → 0.1.2

--- src/django_delayed_union/__init__.py
+++ src/django_delayed_union/__init__.py
@@ -2,7 +2,7 @@ from .difference import DelayedDifferenceQuerySet
 from .intersection import DelayedIntersectionQuerySet
 from .union import DelayedUnionQuerySet
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 __all__ = [
     '__version__',
