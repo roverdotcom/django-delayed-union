a8f7633d1cb49a51194ffc053878733f8ad1072c
Mike Hansen <mike@rover.com>
2019-12-11 14:35:46 +0100

    Update setup.py to reflect new dependencies

--- setup.py
+++ setup.py
@@ -55,8 +55,9 @@ setup(
         'Framework :: Django :: 3.0',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Utilities',
     ],
     keywords=[
