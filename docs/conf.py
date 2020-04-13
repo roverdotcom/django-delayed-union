b4faae250b1d4755691fb0fd20ff59a3e67c0984
Mike Hansen <mike@rover.com>
2020-04-14 01:44:05 +0200

    Bump version: 0.1.4 → 0.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = u'Django Delayed Union'
 year = '2018'
 author = u'Mike Hansen'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.4'
+version = release = '0.1.5'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/django_delayed_union/__init__.py
+++ src/django_delayed_union/__init__.py
@@ -2,7 +2,7 @@ from .difference import DelayedDifferenceQuerySet
 from .intersection import DelayedIntersectionQuerySet
 from .union import DelayedUnionQuerySet
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 __all__ = [
     '__version__',
